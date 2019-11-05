374be72ce7545cd1428e1ac1ac09f3c1fa4488aa
misrra <misrraimsp@gmail.com>
2019-11-05 09:33:23 +0000

    DAOjdbc refactoring

--- src/pfg/firstmarket/dao/DAOjdbc.java
+++ src/pfg/firstmarket/dao/DAOjdbc.java
@@ -24,83 +24,18 @@ public class DAOjdbc implements DAO {
 
 	@Override
 	public List<Book> getBooks() {
-		String sql = "select * from books";
-		List<Book> books = new ArrayList<Book>();
-		
-		try (Connection connexion = DriverManager.getConnection(connRoute,"root","misrra");
-			 Statement stm = connexion.createStatement();
-			 ResultSet rs = stm.executeQuery(sql)) {
-			
-			while(rs.next()) {
-				Book b = new Book(rs.getString("isbn"),rs.getString("title"));
-				books.add(b);
-			}
-		}
-		catch (SQLException e) { e.printStackTrace(); }
-		return books;
+		return fetchBooks("select * from books");
 	}
 
 	@Override
 	public List<Book> getBooks(List<String> conditions) {
-		
-		//building up the query
 		String sql = "select * from books where ";
 		for (String condition : conditions) {
 			sql += condition + " and "; 
 		}
 		sql = sql.substring(0, sql.lastIndexOf(" and "));//el ultimo ' and ' sobra
-		//building up the return value
-		List<Book> books = new ArrayList<Book>();
-		try (Connection connexion = DriverManager.getConnection(connRoute,"root","misrra");
-			 Statement stm = connexion.createStatement();
-			 ResultSet rs = stm.executeQuery(sql)) {
-			
-			while(rs.next()) {
-				Book b = new Book(rs.getString("isbn"),rs.getString("title"));
-				books.add(b);
-			}
-		}
-		catch (SQLException e) { e.printStackTrace(); }
-		return books;
+		return fetchBooks(sql);
 	}
-	
-	/**
-	@Override
-	public List<Book> getBooks(List<String> conditions) {
-		
-		if (conditions.size() == 0) return getAllBooks();
-		System.out.println(conditions.get(0));
-		//conditions management
-		List<String> paramNames = new ArrayList<String>();
-		List<String> paramValues = new ArrayList<String>();
-		String[] parts = null;
-		for (String condition : conditions) {
-			parts = condition.split("=");
-			paramNames.add(parts[0]);
-			paramValues.add(parts[1]);
-		}
-		//building up the query
-		String sql = "select * from books where ";
-		for (String name : paramNames) {
-			sql += name + "=?" + " and "; 
-		}
-		sql = sql.substring(0, sql.lastIndexOf(" and "));//el ultimo ' and ' sobra
-		System.out.println(sql);
-		//building up the return value
-		List<Book> books = new ArrayList<Book>();
-		try (Connection connexion = DriverManager.getConnection(connRoute,"root","misrra");
-			 PreparedStatement stm = getParameterizedQuery(connexion, sql, paramValues);
-			 ResultSet rs = stm.executeQuery(sql)) {
-			
-			while(rs.next()) {
-				Book b = new Book(rs.getString("isbn"),rs.getString("title"));
-				books.add(b);
-			}
-		}
-		catch (SQLException e) { e.printStackTrace(); }
-		return books;
-	}
-	*/
 
 	@Override
 	public void insertBook(Book book) {
@@ -110,12 +45,7 @@ public class DAOjdbc implements DAO {
 		params.add(book.getIsbn());
 		params.add(book.getTitle());
 		
-		try (Connection connexion = DriverManager.getConnection(connRoute,"root","misrra");
-			 PreparedStatement stm = getParameterizedQuery(connexion, sql, params)) {
-			
-			stm.executeUpdate();
-		}
-		catch (SQLException e) { e.printStackTrace(); }
+		update(sql, params);
 	}
 
 	@Override
@@ -125,12 +55,7 @@ public class DAOjdbc implements DAO {
 		params.add(book.getTitle());
 		params.add(book.getIsbn());
 		
-		try (Connection connexion = DriverManager.getConnection(connRoute,"root","misrra");
-			 PreparedStatement stm = getParameterizedQuery(connexion, sql, params)) {
-			
-			stm.executeUpdate();
-		}
-		catch (SQLException e) { e.printStackTrace(); }
+		update(sql, params);
 	}
 	
 	@Override
@@ -139,12 +64,31 @@ public class DAOjdbc implements DAO {
 		List<String> params = new ArrayList<String>();
 		params.add(book.getIsbn());
 		
+		update(sql, params);
+	}
+	
+	private List<Book> fetchBooks(String sql) {
+		List<Book> books = new ArrayList<Book>();
 		try (Connection connexion = DriverManager.getConnection(connRoute,"root","misrra");
-			 PreparedStatement stm = getParameterizedQuery(connexion, sql, params)) {
-			
-			stm.executeUpdate();
-		}
-		catch (SQLException e) { e.printStackTrace(); }
+				 Statement stm = connexion.createStatement();
+				 ResultSet rs = stm.executeQuery(sql)) {
+				
+				while(rs.next()) {
+					Book b = new Book(rs.getString("isbn"),rs.getString("title"));
+					books.add(b);
+				}
+			}
+			catch (SQLException e) { e.printStackTrace(); }
+			return books;
+	}
+	
+	private void update(String sql, List<String> params) {
+		try (Connection connexion = DriverManager.getConnection(connRoute,"root","misrra");
+				 PreparedStatement stm = getParameterizedQuery(connexion, sql, params)) {
+				
+				stm.executeUpdate();
+			}
+			catch (SQLException e) { e.printStackTrace(); }
 	}
 	
 	private static PreparedStatement getParameterizedQuery(Connection connexion, String sql, List<String> params) throws SQLException {
@@ -156,8 +100,6 @@ public class DAOjdbc implements DAO {
 			index++;
 		}
 		return stm;
-}
-
-	
+	}
 
 }
