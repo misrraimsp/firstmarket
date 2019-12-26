8cff937bec930d04b49debb5958cf79067a636dd
misrra <misrraimsp@gmail.com>
2019-12-26 14:38:05 +0000

    cleanup

--- src/pfg/firstmarket/control/actions/Action.java
+++
@@ -1,9 +0,0 @@
-package pfg.firstmarket.control.actions;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Action {
-
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
