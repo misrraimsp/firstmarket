3b98c67a4fc7143f0913afb0f920bd2099fd990f
misrra <misrraimsp@gmail.com>
2019-12-26 14:49:39 +0000

    empty spring project

---
+++ src/main/java/misrraimsp/uned/pfg/firstmarket/FirstmarketApplication.java
@@ -0,0 +1,13 @@
+package misrraimsp.uned.pfg.firstmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/misrraimsp/uned/pfg/firstmarket/FirstmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package misrraimsp.uned.pfg.firstmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
