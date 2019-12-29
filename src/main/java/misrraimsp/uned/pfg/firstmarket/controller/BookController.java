1a22670b1500acc8e18e4faa8026d879574db1c3
misrra <misrraimsp@gmail.com>
2019-12-29 23:29:26 +0000

    Full H2-database CRUD functionality on books

--- src/main/java/misrraimsp/uned/pfg/firstmarket/controller/BookController.java
+++ src/main/java/misrraimsp/uned/pfg/firstmarket/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -22,23 +23,46 @@ public class BookController {
     }
 
     @GetMapping("/admin/books")
-    public String books(Model model){
+    public String showBooks(Model model){
         model.addAttribute("books", bookRepository.findAll());
         return "books";
     }
 
     @GetMapping("/admin/newBook")
-    public String newBook(Model model){
+    public String showNewBook(Model model){
         model.addAttribute("book", new Book());
-        return "newBookForm";
+        return "newBook";
     }
 
     @PostMapping("/admin/newBook")
     public String processNewBook(@Valid Book book, Errors errors){
         if (errors.hasErrors()) {
-            return "newBookForm";
+            return "newBook";
         }
         bookRepository.save(book);
         return "redirect:/admin/books";
     }
+
+    @GetMapping("/admin/editBook/{id}")
+    public String showEditBook(@PathVariable("id") Long id, Model model){
+        Book book = bookRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid book Id: " + id));
+        model.addAttribute("book", book);
+        return "editBook";
+    }
+
+    @PostMapping("/admin/editBook")
+    public String processEditBook(@Valid Book book, Errors errors){
+        if (errors.hasErrors()) {
+            return "editBook";
+        }
+        bookRepository.save(book);
+        return "redirect:/admin/books";
+    }
+
+    @GetMapping("/admin/deleteBook/{id}")
+    public String deleteBook(@PathVariable("id") Long id){
+        bookRepository.deleteById(id);
+        return "redirect:/admin/books";
+    }
+
 }
