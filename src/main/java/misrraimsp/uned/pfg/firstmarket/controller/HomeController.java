a4c79aec11d0b3094919c1d6734f2089e6b0a76d
misrraim <misrraimsp@gmail.com>
2020-02-14 18:57:08 +0000

    still working on layout templates. commonLayout.html as a workbench

--- src/main/java/misrraimsp/uned/pfg/firstmarket/controller/HomeController.java
+++ src/main/java/misrraimsp/uned/pfg/firstmarket/controller/HomeController.java
@@ -1,6 +1,7 @@
 package misrraimsp.uned.pfg.firstmarket.controller;
 
 import misrraimsp.uned.pfg.firstmarket.service.BookServer;
+import misrraimsp.uned.pfg.firstmarket.service.CatServer;
 import misrraimsp.uned.pfg.firstmarket.service.ImageServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,11 +13,13 @@ public class HomeController {
 
     private BookServer bookServer;
     private ImageServer imageServer;
+    private CatServer catServer;
 
     @Autowired
-    public HomeController(BookServer bookServer, ImageServer imageServer) {
+    public HomeController(BookServer bookServer, ImageServer imageServer, CatServer catServer) {
         this.bookServer = bookServer;
         this.imageServer = imageServer;
+        this.catServer = catServer;
     }
 
     @GetMapping({"/", "/home"})
@@ -24,7 +27,9 @@ public class HomeController {
         model.addAttribute("title", "FirstMarket");
         model.addAttribute("logoId", imageServer.getDefaultImageId());
         model.addAttribute("books", bookServer.findAll());
-        return "home";
+        model.addAttribute("indentedCategories", catServer.getIndentedCategories());
+        //return "home";
+        return "commonLayout";
     }
 
     @GetMapping("/login")
