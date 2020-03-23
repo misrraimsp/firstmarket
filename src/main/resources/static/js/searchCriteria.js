46110bfb2790d66462404214397e28fd0d43d3db
misrraim <misrraimsp@gmail.com>
2020-03-23 13:48:00 +0000

    working on searchCriteria.js

--- src/main/resources/static/js/searchCriteria.js
+++ src/main/resources/static/js/searchCriteria.js
@@ -6,63 +6,89 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function() {
-    let i, id, ids, start, end, authorCheckBox;
-    let authorKey = "authorId=";
-    let sendTrigger = document.getElementById("trigger");
-    let authors = document.getElementsByClassName("authorCriteria");
-    let authorAccordion = document.getElementById("author-acc");
-    let url = document.URL;
 
-    start = url.indexOf(authorKey);
-    end = url.indexOf("&", start);
-    if (start != -1){
-        authorAccordion.click();
-        ids = (end == -1) ? url.slice(start + authorKey.length).split(",") : url.slice(start + authorKey.length, end).split(",");
-        for (i = 0; i < ids.length; i++){
-            authorCheckBox = document.getElementById("au-" + ids[i]);
-            if (authorCheckBox == null){
-                //TODO
-            } else {
-                authorCheckBox.checked = true;
+    //definitions
+    let i, j, k, id, ids, fullId, start, end, checkBox, checkBoxes, present;
+    let sendTrigger = document.getElementById("trigger");
+    let url = document.URL;
+    const criteria = [
+        {key:"priceId=", prefix:"pr-", accId:"priceAcc", class:"priceCheckBox"},
+        {key:"authorId=", prefix:"au-", accId:"authorAcc", class:"authorCheckBox"},
+        {key:"publisherId=", prefix:"pu-", accId:"publisherAcc", class:"publisherCheckBox"},
+        {key:"languageId=", prefix:"la-", accId:"languageAcc", class:"languageCheckBox"}
+        ];
+    let regex = new RegExp("," + id + "\$");
+    let extractId = (str) => str.slice(str.indexOf("-") + 1);
+    let extractPrefix = (str) => str.slice(str.search("-") + 1);
+    let extractIds = function (url, key, start, end) {
+        return (end === -1) ? url.slice(start + key.length).split(",") : url.slice(start + key.length, end).split(",");
+    };
+    let extractCriteriaIndex = function(str){
+        for (i = 0; i < criteria.length; i++){
+            if (str.indexOf(criteria[i].prefix) !== -1) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
+    //set all criteria checkboxes checked state based on url info
+    for (i = 0; i < criteria.length; i++){
+        present = false;
+        start = url.indexOf(criteria[i].key);
+        end = url.indexOf("&", start);
+        if (start !== -1){
+            ids = extractIds(url, criteria[i].key, start, end);
+            for (j = 0; j < ids.length; j++){
+                checkBox = document.getElementById(criteria[i].prefix + ids[j]);
+                if (checkBox != null){
+                    checkBox.checked = true;
+                    present = true;
+                }
+            }
+            if (present) {
+                document.getElementById(criteria[i].accId).click();
             }
         }
     }
 
-    for (i = 0; i < authors.length; i++) {
-        authors[i].addEventListener("change", function (e) {
-            authorCheckBox = e.currentTarget;
-            id = authorCheckBox.getAttribute("id");
-            id = id.slice(id.indexOf("-") + 1);
-            start = url.indexOf(authorKey);
-            end = url.indexOf("&", start);
-            if (authorCheckBox.checked){
-                if (start != -1){
-                    ids = (end == -1) ? url.slice(start) : url.slice(start, end);
-                    url = url.replace(ids, ids + "," + id);
+    //set all criteria checkboxes 'change' event listener
+    for (i = 0; i < criteria.length; i++){
+        checkBoxes = document.getElementsByClassName(criteria[i].class);
+        for (j = 0; j < checkBoxes.length; j++) {
+            checkBoxes[j].addEventListener("change", function (e) {
+                checkBox = e.currentTarget;
+                fullId = checkBox.getAttribute("id");
+                id = extractId(fullId);
+                k = extractCriteriaIndex(fullId);
+                start = url.indexOf(criteria[k].key);
+                end = url.indexOf("&", start);
+                if (checkBox.checked){
+                    if (start !== -1){
+                        ids = (end === -1) ? url.slice(start) : url.slice(start, end);
+                        url = url.replace(ids, ids + "," + id);
+                    } else {
+                        url += (url.indexOf("?") !== -1) ? "&" + criteria[k].key + id : "?" + criteria[k].key + id;
+                    }
                 } else {
-                    url += (url.indexOf("?") != -1) ? "&" + authorKey + id : "?" + authorKey + id;
+                    ids = extractIds(url, criteria[k].key, start, end);
+                    if (ids.length > 1){
+                        url = url.
+                        replace("=" + id + ",", "=").
+                        replace("," + id + ",", ",").
+                        replace("," + id + "&", "&").
+                        replace(regex, "");
+                    } else {
+                        url = url.
+                        replace("&" + criteria[k].key + id, "").
+                        replace("?" + criteria[k].key + id + "&", "?").
+                        replace("?" + criteria[k].key + id, "");
+                    }
                 }
-            } else {
-                ids = (end == -1) ? url.slice(start + authorKey.length).split(",") : url.slice(start + authorKey.length, end).split(",");
-                if (ids.length > 1){
-                    let re = new RegExp("," + id + "\$");
-                    url = url.
-                    replace("=" + id + ",", "=").
-                    replace("," + id + ",", ",").
-                    replace("," + id + "&", "&").
-                    replace(re, "");
-                } else {
-                    url = url.
-                    replace("&" + "authorId=" + id, "").
-                    replace("?" + "authorId=" + id + "&", "?").
-                    replace("?" + "authorId=" + id, "");
-                }
-            }
-
-            sendTrigger.setAttribute("href", url);
-            sendTrigger.click();
-
-        }, false);
+                sendTrigger.setAttribute("href", url);
+                sendTrigger.click();
+            }, false);
+        }
     }
 
 }, false);
