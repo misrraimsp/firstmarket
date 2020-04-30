7d843c63e0c04919a5faf25dd9dfce2aa076fca2
misrra <misrraimsp@gmail.com>
2020-04-30 18:34:36 +0100

    working on ajaxCart.js

--- src/main/resources/static/js/ajaxCart.js
+++ src/main/resources/static/js/ajaxCart.js
@@ -6,43 +6,127 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    let ajaxObject, i, button;
 
+    // variables
+    let ajaxObject, i, element, action, id, triggerElement, url;
+    let idsToRemove = '';
+    let baseUrl = "http://localhost:8080/firstmarket/ajaxCart/";
+    let snackbar = document.getElementById("snackbar");
+    let snackbarTimeout = 3000;
+
+    // functions
     let extractNumber = (str) => str.slice(str.indexOf("-") + 1);
 
-    //addBook
-    let addBookButtons = document.getElementsByClassName("addBook");
-    for (i = 0; i < addBookButtons.length; i++){
-        button = addBookButtons[i];
-        button.addEventListener("click", function(e) {
+    let onHttpOk = function(action, id, q) {
+
+        let updatePrices = function(id) {
+
+        };
+
+        document.getElementById("cartIcon").setAttribute("data-count", q);
+        switch (action) {
+            case 'addBook':
+                // snackbar
+                snackbar.className = "show";
+                setTimeout(function() {
+                    snackbar.className = snackbar.className.replace("show", "");
+                    }, snackbarTimeout);
+                break;
+            case 'incrementItem':
+                document.getElementById("numOfBooks").innerHTML = q;
+                document.getElementById("iq-" + id).innerHTML++;
+                updatePrices(id);
+                break;
+            case 'decrementItem':
+                document.getElementById("numOfBooks").innerHTML = q;
+                let qElement = document.getElementById("iq-" + id);
+                if (Number(qElement.innerHTML) > 1){
+                    qElement.innerHTML--;
+                } else {
+                    document.getElementById("i-" + id).style.display = "none";
+                }
+                break;
+            case 'removeItems':
+                document.getElementById("numOfBooks").innerHTML = q;
+                //document.getElementById("i-" + id).style.display = "none";
+                break;
+            default:
+                console.log("error state within onHttpOk function: no action found");
+        }
+    };
+
+    let onHttpUnauthorized = function() {
+        document.getElementById("loginLink").click();
+    };
+
+    // ajax actions
+    let ajaxElements = document.querySelectorAll("[ajax]");
+    for (i = 0; i < ajaxElements.length; i++) {
+        element = ajaxElements[i];
+        element.addEventListener("click", function(e) {
+            triggerElement = e.currentTarget;
+            action = triggerElement.getAttribute("ajax");
+            if (action == 'removeItems') {
+                id = null;
+                url = baseUrl + action +"?ids=" + idsToRemove;
+            }
+            else {
+                id = extractNumber(triggerElement.getAttribute("id"));
+                url = baseUrl + action +"/" + id;
+            }
             ajaxObject = new XMLHttpRequest();
-            let id = extractNumber(e.currentTarget.getAttribute("id"));
-            ajaxObject.open(
-                "GET",
-                "http://localhost:8080/firstmarket/ajaxCart/addBook/" + id,
-                true);
+            ajaxObject.open("GET", url, true);
             ajaxObject.setRequestHeader('isAjaxCartRequested', '1');
             ajaxObject.send();
             ajaxObject.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("cart").setAttribute("data-count", this.responseText);
-                    // snackbar
-                    let sbar = document.getElementById("snackbar");
-                    sbar.className = "show";
-                    setTimeout(function(){ sbar.className = sbar.className.replace("show", ""); }, 3000);
+                    onHttpOk(action, id, this.responseText);
                 }
                 if (this.readyState == 4 && this.status == 401) {
-                    document.getElementById("loginLink").click();
+                    onHttpUnauthorized();
                 }
             };
         }, false);
     }
+}, false);
 
+
+
+/*
+for (i = 0; i < addBookButtons.length; i++){
+    button = addBookButtons[i];
+    button.addEventListener("click", function(e) {
+        ajaxObject = new XMLHttpRequest();
+        let id = extractNumber(e.currentTarget.getAttribute("id"));
+        ajaxObject.open(
+            "GET",
+            baseUrl + "/addBook/" + id,
+            true);
+        ajaxObject.setRequestHeader('isAjaxCartRequested', '1');
+        ajaxObject.send();
+        ajaxObject.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                onHttpOk('addBook', id, this.responseText);
+                //document.getElementById("cart").setAttribute("data-count", this.responseText);
+                // snackbar
+                //let sbar = document.getElementById("snackbar");
+                //sbar.className = "show";
+                //setTimeout(function(){ sbar.className = sbar.className.replace("show", ""); }, 3000);
+            }
+            if (this.readyState == 4 && this.status == 401) {
+                onHttpUnauthorized();
+            }
+        };
+    }, false);
+}
+
+ */
+/*
     //incrementItem
     let incrementItemButtons = document.getElementsByClassName("incrementItem");
     for (i = 0; i < incrementItemButtons.length; i++){
-        button = incrementItemButtons[i];
-        button.addEventListener("click", function(e) {
+        element = incrementItemButtons[i];
+        element.addEventListener("click", function(e) {
             ajaxObject = new XMLHttpRequest();
             let id = extractNumber(e.currentTarget.getAttribute("id"));
             ajaxObject.open(
@@ -53,8 +137,7 @@ document.addEventListener("DOMContentLoaded", function() {
             ajaxObject.send();
             ajaxObject.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("cart").setAttribute("data-count", this.responseText);
-                    document.getElementById("iq-" + id).innerHTML++;
+                    onHttpOk(id);
                 }
                 if (this.readyState == 4 && this.status == 401) {
                     document.getElementById("loginLink").click();
@@ -62,12 +145,13 @@ document.addEventListener("DOMContentLoaded", function() {
             };
         }, false);
     }
-
+*/
+/*
     //decrementItem
     let decrementItemButtons = document.getElementsByClassName("decrementItem");
     for (i = 0; i < decrementItemButtons.length; i++){
-        button = decrementItemButtons[i];
-        button.addEventListener("click", function(e) {
+        element = decrementItemButtons[i];
+        element.addEventListener("click", function(e) {
             ajaxObject = new XMLHttpRequest();
             let id = extractNumber(e.currentTarget.getAttribute("id"));
             ajaxObject.open(
@@ -92,12 +176,13 @@ document.addEventListener("DOMContentLoaded", function() {
             };
         }, false);
     }
-
+*/
+/*
     //removeItem
     let removeItemButtons = document.getElementsByClassName("removeItem");
     for (i = 0; i < removeItemButtons.length; i++){
-        button = removeItemButtons[i];
-        button.addEventListener("click", function(e) {
+        element = removeItemButtons[i];
+        element.addEventListener("click", function(e) {
             ajaxObject = new XMLHttpRequest();
             let id = extractNumber(e.currentTarget.getAttribute("id"));
             ajaxObject.open(
@@ -117,5 +202,4 @@ document.addEventListener("DOMContentLoaded", function() {
             };
         }, false);
     }
-
-}, false);
+*/
