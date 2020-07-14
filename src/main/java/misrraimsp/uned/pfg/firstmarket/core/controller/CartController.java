6dac731750bdb72fb203acd9d09777a5ef115891
misrra <misrraimsp@gmail.com>
2020-07-14 19:11:33 +0100

    cors disabled

--- src/main/java/misrraimsp/uned/pfg/firstmarket/core/controller/CartController.java
+++ src/main/java/misrraimsp/uned/pfg/firstmarket/core/controller/CartController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +19,6 @@ import java.security.Principal;
 import java.util.Set;
 
 @RestController
-@CrossOrigin
 public class CartController extends BasicController {
 
     private final CartServer cartServer;
