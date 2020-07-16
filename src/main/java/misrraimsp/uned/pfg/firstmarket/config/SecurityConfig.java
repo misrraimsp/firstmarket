98caa879dce13dc26e6d8afcc8ba79f72f001fe0
misrra <misrraimsp@gmail.com>
2020-07-16 10:48:57 +0100

    prod config

--- src/main/java/misrraimsp/uned/pfg/firstmarket/config/SecurityConfig.java
+++ src/main/java/misrraimsp/uned/pfg/firstmarket/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -58,18 +59,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .ignoringAntMatchers("/listener") // open for stripe notifications
-                .ignoringAntMatchers("/h2-console/**") // Make H2-Console non-secured; for debug purposes
+                //.ignoringAntMatchers("/h2-console/**") // Make H2-Console non-secured; for debug purposes
 
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
 
-                // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+
                 .and()
                 .headers()
-                //.referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN)
-                .frameOptions()
-                .sameOrigin()
+                .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN)
+                // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+                //.frameOptions()
+                //.sameOrigin()
         ;
     }
 
