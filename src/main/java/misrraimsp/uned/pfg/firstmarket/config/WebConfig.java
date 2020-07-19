27b25471a8f5ff57e8c3b0cb05b67f17c7cd81d9
misrra <misrraimsp@gmail.com>
2020-07-19 13:09:54 +0100

    -report increment -trying to cache static content

--- src/main/java/misrraimsp/uned/pfg/firstmarket/config/WebConfig.java
+++ src/main/java/misrraimsp/uned/pfg/firstmarket/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -49,6 +50,13 @@ public class WebConfig implements WebMvcConfigurer {
         LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
         bean.setValidationMessageSource(messageSource());
         return bean;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(31556926);
     }
 
 }
