8a2b0b5a3e878355a4639b371e009b66bb82b2c2
misrraim <misrraimsp@gmail.com>
2020-07-23 10:23:45 +0100

    working on inline style suppression

--- src/main/java/misrraimsp/uned/pfg/firstmarket/config/WebConfig.java
+++ src/main/java/misrraimsp/uned/pfg/firstmarket/config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -52,11 +51,14 @@ public class WebConfig implements WebMvcConfigurer {
         return bean;
     }
 
+    /*
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .setCachePeriod(31556926);
     }
+
+     */
 
 }
